b9c0ba86b5280548a0c44c09a13d90d22e9f87f4
Sweta700 <swetamaurya1193@gmail.com>
2025-08-01 10:09:35 -0400

    Update BubbleSort.java

--- New folder/Accending.java
+++ New folder/Accending.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Accending {
+public class BubbleSort {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
        
@@ -11,17 +11,17 @@ public class Accending {
     for(int i=0;i<size;i++){
         a[i]=sc.nextInt(); 
     }
-    for(int i=0;i<size;i++){
-        for(int j=i+1;j<size;j++){
-        if(a[i]>a[j]){
-        int temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size-1-i; j++){
+        if(a[j] > a[j+1]){
+        int temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
         }
     }
     }
     System.out.println("Your element in accending order");
-    for(int i=0;i<size;i++){
+    for(int i=0; i<size; i++){
         System.out.print(a[i]+" ");
              sc.close();
    
